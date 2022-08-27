45ecdeb24623b00ab16bebf14f9c0cd84bab92e0
NguyenHaiLy02 <nguyenhaily1014@gmail.com>
2022-08-27 18:00:56 +0700

    bt

---
+++ app/src/main/java/com/example/tuan1_baitap1_nguyenhaily/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tuan1_baitap1_nguyenhaily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
